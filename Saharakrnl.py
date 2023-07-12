4bed3c18695d4c44a4170915949764795b8ea6f6
SizzlRetreat <103748542+SizzlRetrat@users.noreply.github.com>
2023-07-12 16:34:21 +0100

    Create Saharakrnl.py

---
+++ Saharakrnl.py
@@ -0,0 +1,16 @@
+hi = randint(1,10)
+print hi
+sleep(50ms)
+print"
+---------------ExbosSahara v1.1.1 fully loaded---------------
+---------------ExbosSahara is copyright EXBINC---------------
+-----------------build 1.1.1 genuine github -----------------
+------------Loading your desktop, wait 10 seconds------------
+
+
+
+------------------------buildco 1010-------------------------
+"
+
+sleep(1000ms)
+open(/ExbosSahara/login.py)
