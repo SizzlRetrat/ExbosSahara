f73c84bde21dfc71d279b18ab1074832e04c8e01
SizzlRetreat <103748542+SizzlRetrat@users.noreply.github.com>
2023-07-12 17:06:55 +0100

    Login added at ExbosSahara/login.py

--- ExbosSahara/login.py
+++ ExbosSahara/login.py
@@ -1,1 +1,13 @@
+acc = randint(1,100)
+print acc
+wait(50ms)
+yourlogin2023 = Githubuser@Exbos
+print yourlogin2023
+wait(50ms)
+print "please login to Githubuser@exbos"
+if user == "0000"
+open(/ExbosSahara/main.py)
+else:
+  print "wrong password go to reboot"
+
 
