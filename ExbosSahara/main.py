05c1af86482fe1c3f25f28d992db76b43c89bdb7
SizzlRetreat <103748542+SizzlRetrat@users.noreply.github.com>
2023-07-13 16:52:32 +0100

    Update main.py

--- ExbosSahara/main.py
+++ ExbosSahara/main.py
@@ -1,7 +1,9 @@
 print "Welcome to ExbosSahara build v1.1.1
 Type GAME to go to a game Type BUILD to view your 
 Build Type CommandApplication to run command prompt 
-Type EXBGC to view the specs of the EXBGC"
+Type EXBGC to view the specs of the EXBGC Type 
+MyApps to run your apps beware you have to code
+in your apps"
 
 if user == "GAME"
 open(/Games/Games.py)
@@ -16,6 +18,7 @@ print "The EXBGC runs on a pi zero w see specsheet at
 raspberrypi.com"
 sleep(50ms)
 def repeat(1)
-
+if user == "MyApps"
+open(/PersonalApps/control.py)
 
 
