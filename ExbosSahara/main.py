80e7703e24ebd699a6016a91a623975da1c10b2b
SizzlRetreat <103748542+SizzlRetrat@users.noreply.github.com>
2023-07-13 16:17:17 +0100

    Update main.py (still not finished)

--- ExbosSahara/main.py
+++ ExbosSahara/main.py
@@ -1,3 +1,13 @@
 print "Welcome to ExbosSahara build v1.1.1
+Type GAME to go to a game Type BUILD to view your 
+Build Type CommandApplication to run command prompt 
+Type EXBGC to view the specs of the EXBGC"
+
+if user == "GAME"
+open(/Games/Games.py)
+if user == "BUILD"
+print "BUILD V1.1.1"
+def repeat(1)
+
 
 
