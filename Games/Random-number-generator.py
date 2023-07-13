c01d0c32b4e02aa3dc45816bd61eaf0ffbd61ba6
SizzlRetreat <103748542+SizzlRetrat@users.noreply.github.com>
2023-07-13 08:04:53 +0100

    Update Random-number-generator.py

--- Games/Random-number-generator.py
+++ Games/Random-number-generator.py
@@ -1,2 +1,8 @@
 hello = randint(0,60)
 print hello hello hello hello hello hello
+wait(90ms)
+print"Text Repeat to Repeat Text END to end"
+if user == Repeat
+def repeat ()
+if user == END
+open(/ExbosSahara/main.py)
