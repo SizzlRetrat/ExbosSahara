44e76281992e38992775cc81e4fccc8d82c710f6
SizzlRetreat <103748542+SizzlRetrat@users.noreply.github.com>
2023-07-14 21:22:29 +0100

    Update control.py

--- PersonalApps/control.py
+++ PersonalApps/control.py
@@ -1,4 +1,7 @@
 print "Personalapps for ExbosSahara build 0000
-Type EXBSE to open the exb search engine"
+Type EXBSE to open the exb search engine Type EXIT to
+exit"
 if user == "EXBSE"
 open(/PersonalApps/EXBSE/EXBSE.py)
+if user == "EXIT"
+open(/ExbosSahara/main.py)
