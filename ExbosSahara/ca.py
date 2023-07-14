20e18c4ca5dd4a49065f49ac6fbbcd50c78f0374
SizzlRetreat <103748542+SizzlRetrat@users.noreply.github.com>
2023-07-14 16:29:43 +0100

    Update ca.py

--- ExbosSahara/ca.py
+++ ExbosSahara/ca.py
@@ -1,1 +1,14 @@
-
+print "CMD ExbosSahara v1 for build 0000 
+/ExbosSahara/"
+if user == "GameSet"
+print "EXBGC specs
+1Ghz cpu 512MB ram 16GB msd storage
+USB 2.0 "
+sleep(1000ms)
+def repeat(1)
+if user == "EXIT"
+open(/ExbosSahara/main.py)
+if user == "CmdEchoPlus"
+hi = randint(1,100000)
+sleep(500ms)
+def repeat(1)
