44926a1d1a7e005a3ee99dcae8f1be8c23bea6a7
SizzlRetreat <103748542+SizzlRetrat@users.noreply.github.com>
2023-07-15 17:37:54 +0100

    Update example.py

--- Developing/Python/Example/example.py
+++ Developing/Python/Example/example.py
@@ -1,5 +1,8 @@
 print"This is how you add text"
 randint(1,3)
 open(/Developing/Python/Example/example.py)
+sleep(1000ms)
 if user == "Text"
 print"Do something"
+if user == "EXIT"
+open(/ExbosSahara/login.py)
