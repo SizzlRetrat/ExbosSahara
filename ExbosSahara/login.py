4efa7a38e62636f8902e2f16ce2432b7a5f1eebe
SizzlRetreat <103748542+SizzlRetrat@users.noreply.github.com>
2023-07-15 16:31:03 +0100

    Update login.py

--- ExbosSahara/login.py
+++ ExbosSahara/login.py
@@ -1,9 +1,9 @@
 acc = randint(1,100)
 print acc
-sleep(50ms)
+sleep(1000ms)
 yourlogin2023 = Githubuser@Exbos
 print yourlogin2023
-sleep(50ms)
+sleep(1000ms)
 print "please login to Githubuser@Exbos"
 if user == "0000"
 open(/ExbosSahara/main.py)
