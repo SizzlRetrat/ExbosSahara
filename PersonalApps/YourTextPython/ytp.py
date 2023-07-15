7f984d60a608d2aad3831847522ec7c7c7fc0599
SizzlRetreat <103748542+SizzlRetrat@users.noreply.github.com>
2023-07-15 13:37:02 +0100

    Update ytp.py

--- PersonalApps/YourTextPython/ytp.py
+++ PersonalApps/YourTextPython/ytp.py
@@ -1,3 +1,3 @@
-print"Welcome to YTP v1 for Exbos, Type LEARN to learn YTP"
+print"Welcome to YTP v1 for Exbos, Type LEARN to learn YTP. YTP IS NOT READY YET UPDATE TO BUILD 0010 TO USE YTP"
 if user == "LEARN"
 open(/PersonalApps/YourTextPython/LearnYTP.py)
