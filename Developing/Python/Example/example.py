63682c089a1c2f4d9d3fb6099b0f774e0ffbfaf4
SizzlRetreat <103748542+SizzlRetrat@users.noreply.github.com>
2023-07-15 17:38:33 +0100

    Delete Developing/Python/Example directory

--- Developing/Python/Example/example.py
+++
@@ -1,8 +0,0 @@
-print"This is how you add text"
-randint(1,3)
-open(/Developing/Python/Example/example.py)
-sleep(1000ms)
-if user == "Text"
-print"Do something"
-if user == "EXIT"
-open(/ExbosSahara/login.py)
