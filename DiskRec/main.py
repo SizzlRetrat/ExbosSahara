708ba8ea5c0749a8109eb44d84847305f4e268e0
SizzlRetreat <103748542+SizzlRetrat@users.noreply.github.com>
2023-07-15 16:17:09 +0100

    Update main.py

--- DiskRec/main.py
+++ DiskRec/main.py
@@ -1,1 +1,1 @@
-print"Welcome To DiskRec Main, this feature will NOT be added untill build 0011 (build 2 of v1)"
+print"Welcome To DiskRec Main, this feature will NOT be added untill build 0010 (build 2 of v1)"
