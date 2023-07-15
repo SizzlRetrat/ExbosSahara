750b599bc9dbc5d51009fcf7d012e43c77a0536d
SizzlRetreat <103748542+SizzlRetrat@users.noreply.github.com>
2023-07-15 12:46:01 +0100

    Update Games.py

--- Games/Games.py
+++ Games/Games.py
@@ -1,1 +1,6 @@
-
+print " Welcome to GameStation on ExbosSahara!
+Random Number Generator: rand Exit: EXIT"
+if user == "EXIT"
+open(/ExbosSahara/main.py)
+if user == "rand"
+open(/Games/Random-Number-Generator.py)
