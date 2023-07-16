f047deb58a78573c31dd41632431b13169f4c099
SizzlRetreat <103748542+SizzlRetrat@users.noreply.github.com>
2023-07-16 13:47:26 +0100

    Create GraphicsTester.py

---
+++ Games/GraphicsTester.py
@@ -0,0 +1,47 @@
+import turtle
+screen = turtle.Screen()
+screen.title("GraphicsTester")
+screen.bgcolor("lightblue")
+
+Tester1 = turtle.Turtle()
+Tester1.shape("circle")
+Tester1.color("green")
+Tester1.shapesize(0.5, 4)
+Tester1.penup()
+Tester1.goto(-300, 0)
+
+Tester2 = turtle.Turtle()
+Tester2.shape("triangle")
+Tester2.color("lightblue")
+Tester2.shapesize(10, 4)
+Tester2.penup()
+Tester2.goto(-100, 20)
+Tester2.setheading(90)
+
+print"Type Exit to leave, Text Test to Test"
+
+if user == "Exit"
+open(/ExbosSahara/main.py)
+
+if user == "Test"
+Tester1.goto(-200, -10)
+sleep(1ms)
+Tester2.goto(0, 10)
+sleep(1000ms)
+Tester1.goto(-100, 0)
+sleep(1ms)
+Tester2.goto(100, 0)
+sleep(1000ms)
+Tester1.goto(0, -100)
+sleep(1ms)
+Tester2.goto(200, 100)
+sleep(1000ms)
+Tester1.goto(100, 0)
+sleep(1ms)
+Tester2.goto(300, 200)
+sleep(2000ms)
+print"if you got to the end you can run any of the games on ExbosSahara"
+sleep(2000ms)
+open(/ExbosSahara/main.py)
+
+
