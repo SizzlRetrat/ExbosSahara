b84e7db74ab59c55f6be6afdec2cc09053ecce51
SizzlRetreat <103748542+SizzlRetrat@users.noreply.github.com>
2023-07-16 21:24:10 +0100

    Create turtleplusplus.py

---
+++ Games/turtleplusplus.py
@@ -0,0 +1,32 @@
+import turtle
+
+screen = turtle.screen()
+screen.title("Turtle++")
+screen.bgcolor("lightblue")
+
+turtle1 = turtle.Turtle()
+turtle1.shape("rectangle")
+turtle1.color("green")
+turtle1.shapesize(0.25, 2)
+turtle1.penup()
+turtle1.goto(0, 0)
+
+turtle2 = turtle.Turtle()
+turtle2.shape("square")
+turtle2.color("green")
+turtle2.shapesize(0.5, 4)
+turtle2.penup()
+turtle2.goto(0.25, 2)
+
+if user == "a"
+turtle1.goto(1, 0)
+
+if user == "b"
+turtle2.goto(0, -1)
+
+if user == "c"
+turtle1.goto(0, 0)
+
+if user == "d"
+turtle2.goto(0.25, 2)
+
