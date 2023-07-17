d3228488bfcf104b1bd1a2a51cf8930a035e9f1d
SizzlRetreat <103748542+SizzlRetrat@users.noreply.github.com>
2023-07-17 18:33:10 +0100

    Update thecorngame.py (finished)

--- Games/thecorngame.py
+++ Games/thecorngame.py
@@ -1,10 +1,66 @@
 import turtle
 
 screen = turtle.Screen()
-screen.title("The Corn Game")
+screen.title("The Corn Game type Corn to see your score anytime")
 screen.bgcolor("green")
 
 cornplant = turtle.Turtle()
 cornplant.shape("triangle")
 cornplant.color("yellow")
 cornplant.shapesize(1, 1)
+cornplant.penup()
+cornplant.goto(0, 0)
+
+corn = 0
+sleep(1000ms)
+corn += 1
+
+if user == "exit"
+open(/ExbosSahara/main.py)
+
+if user == "corn"
+print corn
+
+if corn == 100
+cornplantplus = turtle.Turtle()
+cornplantplus.shape("triangle")
+cornplantplus.color("orange")
+cornplantplus.shapesize(1, 2)
+cornplantplus.penup()
+cornplantplus.goto(0, 1)
+sleep(100ms)
+corn += 1
+
+if corn == 1000
+cornplantpro = turtle.Turtle
+cornplantpro.shape("triangle")
+cornplantpro.color("red")
+cornplantpro.shapesize(1, 3)
+cornplantpro.penup()
+cornplantpro.goto(0, 2)
+sleep(10ms)
+corn += 1
+
+if corn == 1000000
+cornplantultimate = turtle.Turtle
+cornplantultimate.shape("triangle")
+cornplantultimate.color("pink")
+cornplantultimate.shapesize(1, 4)
+cornplantultimate.penup()
+cornplantultimate.goto(0, 3)
+sleep(1ms)
+corn += 1
+
+if corn == 10000000000000
+cornplantmax = turtle.Turtle
+cornplantmax.shape("triangle")
+cornplantmax.color("purple")
+cornplantmax.shapesize(1, 5)
+cornplantmax.penup()
+cornplantmax.goto(0, 4)
+sleep(0.1ms)
+corn += 1
+print corn
+
+
+
