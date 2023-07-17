64768b6f7bc922dc3312b6816952978eb1a1b62e
SizzlRetreat <103748542+SizzlRetrat@users.noreply.github.com>
2023-07-17 21:12:22 +0100

    Create untitledgame.py

---
+++ Games/untitledgame.py
@@ -0,0 +1,27 @@
+import turtle
+
+screen = turtle.Screen
+screen.title("untitled game")
+screen.bgcolor("lightblue")
+
+untitled1 = turtle.Turtle
+untitled1.shape("rectangle")
+untitled1.color("lightblue")
+untitled1.shapesize(1, 3)
+untitled1.penup()
+untitled1.goto(0, 0)
+
+if user == "a"
+untitled1.goto(0, -1)
+
+if user == "d"
+untitled1.goto(0, 1)
+
+if user == "s"
+untitled1.goto(-1, 0)
+
+if user == "w"
+untitled1.goto(1, 0)
+
+if user == "exit"
+open(/ExbosSahara/main.py)
