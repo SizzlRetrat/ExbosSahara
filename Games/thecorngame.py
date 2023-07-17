b6a02233f67a0905a9535ede7d85483cf1511c7b
SizzlRetreat <103748542+SizzlRetrat@users.noreply.github.com>
2023-07-17 18:09:20 +0100

    Update thecorngame.py

--- Games/thecorngame.py
+++ Games/thecorngame.py
@@ -1,3 +1,10 @@
 import turtle
+
+screen = turtle.Screen()
+screen.title("The Corn Game")
+screen.bgcolor("green")
+
 cornplant = turtle.Turtle()
 cornplant.shape("triangle")
+cornplant.color("yellow")
+cornplant.shapesize(1, 1)
