465d69ff34652eebad448ff717223b5710c021eb
SizzlRetreat <103748542+SizzlRetrat@users.noreply.github.com>
2023-07-22 11:57:34 +0100

    Create imager.py (not ready)

---
+++ imager/imager.py
@@ -0,0 +1,213 @@
+print("hello welcome to the ExbosSahara imager, the auto username is githubuser and the password is 0000")
+sleep(100ms)
+open(Saharakrnl.py 'w')
+write(Saharakrnl.py "
+ hi = randint(1,10)
+ print hi
+ sleep(1000)
+ print "
+ -----------------ExbosSahara v1 fully loaded-----------------
+ ---------------ExbosSahara is copyright EXBINC---------------
+ --------------------build 0000  genuine  --------------------
+ ------------Loading your desktop, wait 10 seconds------------
+
+ -------------Type BOOT to Boot into ExbosSahara--------------
+
+ ------------------------buildco 0000-------------------------
+ "
+
+ if user == "BOOT"
+ open(/ExbosSahara/login.py)
+ if user == "DiskRec"
+ open(/DiskRec/DiskRec.py)
+ if user == "GamesPreview"
+ print"CurrentDateTime"
+ sleep(1000ms)
+ def repeat(1)
+ if user == "a22"
+ print"ExbosSahara v1 build 0000 (c) EXB Technologies Inc 2023"
+ ")
+open(/ExbosSahara/login.py 'w')
+write(/ExbosSahara/login.py "
+ acc = randint(1,100)
+ print (acc)
+ sleep(1000ms)
+ yourlogin2023 = Githubuser@Exbos
+ print (yourlogin2023)
+ sleep(1000ms)
+ print ("please login to Githubuser@Exbos")
+ if user == "0000"
+  open(/ExbosSahara/main.py)
+ else:
+  print  ("wrong password go to reboot")
+	 ")
+open(/ExbosSahara/main.py 'w')
+write(/ExbosSahara/main.py "
+ print("Welcome to ExbosSahara build v1
+ Type GAME to go to a game Type BUILD to view your 
+ Build Type CommandApplication to run command prompt 
+ Type EXBGC to view the specs of the EXBGC Type 
+ MyApps to run your apps beware you have to code
+ in your apps")
+
+ if user == "GAME"
+ open(/Games/Games.py)
+ if user == "BUILD"
+ print "BUILD V1"
+ sleep(1000ms)
+ def repeat(1)
+ if user == "CommandApplication"
+ open(/ExbosSahara/ca.py)
+ if user == "EXBGC"
+ print "The EXBGC runs on a pi zero w see specsheet at
+ raspberrypi.com"
+ sleep(50ms)
+ def repeat(1)
+ if user == "MyApps"
+ open(/PersonalApps/control.py)
+ if user == "RegEdit"
+ open(/ExbosSahara/ReferenceReg/reg.py)
+ if user == "BuildStatus"
+ print "Build 0000"
+ sleep(2000ms)
+ def repeat(1)
+ if user == "YTP"
+ open(/PersonalApps/YourTextPython/ytp.py)
+ if user == "PythonExample"
+ open(/Developing/Python/example.py)
+ if user == "HtmlExample"
+ open(/Developing/HTML/example.html)
+ if user == "shutdown"
+ os.system(shutdown /s /t 1)
+ ")
+ 
+open(/ExbosSahara/ca.py 'w')
+write(/ExbosSahara/ca.py "
+ print "CMD ExbosSahara v1 for build 0000 
+ /ExbosSahara/"
+ if user == "GameSet"
+ print "EXBGC specs
+ 1Ghz cpu 512MB ram 16GB msd storage
+ USB 2.0 "
+ sleep(10000ms)
+ def repeat(1)
+ if user == "EXIT"
+ open(/ExbosSahara/main.py)
+ if user == "CmdEchoPlus"
+ hi = randint(1,100000)
+ sleep(1000ms)
+ def repeat(1)
+ ")
+open(/ExbosSahara/ReferenceReg/reg.py 'w'
+write(/ExbosSahara/ReferenceReg/reg.py "
+ print("cool")
+ ")
+open(/Games/games.py 'w')
+write(/Games/games.py "
+ print " Welcome to GameStation on ExbosSahara!
+ Random Number Generator: rand    Exit: EXIT Arc: Archery Benchmark: Benchmark Turtle++: turtle untitled game: untitled cricket game: cricket"
+ if user == "EXIT"
+ open(/ExbosSahara/main.py)
+ if user == "rand"
+ open(/Games/Random-Number-Generator.py)
+ if user == "Test"
+ open(/Games/gamestest.py)
+ if user == "Arc"
+ open(/Games/archery.py)
+ if user == "Benchmark"
+ open(/Games/GraphicsTester.py)
+ if user == "turtle"
+ open(/Games/turtleplusplus.py)
+ if user == "untitled"
+ open(/Games/untitledgame.py)
+ if user == "cricket"
+ open(/Games/cricket.py)
+ ")
+open(/Games/GraphicsTester.py 'w')
+write(/Games/GraphicsTester.py "
+ import turtle
+ screen = turtle.Screen()
+ screen.title("GraphicsTester")
+ screen.bgcolor("lightblue")
+
+ Tester1 = turtle.Turtle()
+ Tester1.shape("circle")
+ Tester1.color("green")
+ Tester1.shapesize(0.5, 4)
+ Tester1.penup()
+ Tester1.goto(-300, 0)
+
+ Tester2 = turtle.Turtle()
+ Tester2.shape("triangle")
+ Tester2.color("lightblue")
+ Tester2.shapesize(10, 4)
+ Tester2.penup()
+ Tester2.goto(-100, 20)
+ Tester2.setheading(90)
+
+ print"Type Exit to leave, Text Test to Test"
+
+ if user == "Exit"
+ open(/ExbosSahara/main.py)
+
+ if user == "Test"
+ Tester1.goto(-200, -10)
+ sleep(1ms)
+ Tester2.goto(0, 10)
+ sleep(1000ms)
+ Tester1.goto(-100, 0)
+ sleep(1ms)
+ Tester2.goto(100, 0)
+ sleep(1000ms)
+ Tester1.goto(0, -100)
+ sleep(1ms)
+ Tester2.goto(200, 100)
+ sleep(1000ms)
+ Tester1.goto(100, -200)
+ sleep(1ms)
+ Tester2.goto(300, 200)
+ sleep(1ms)
+ Tester1.goto(200, -100)
+ sleep(1ms)
+ Tester2.goto(400, 300)
+ sleep(1000ms)
+ Tester1.goto(200, 0)
+ sleep(1ms)
+ Tester2.goto(500, 400)
+ sleep(1000ms)
+ Tester1.goto(300, 100)
+ sleep(1ms)
+ Tester2.goto(600, 500)
+ sleep(1000ms)
+ Tester1.goto(400, 200)
+ sleep(1ms)
+ Tester2.goto(700, 600)
+ sleep(1000ms)
+ Tester1.goto(500, 300)
+ sleep(1ms)
+ Tester2.goto(800, 700)
+ sleep(1000ms)
+ Tester1.goto(600, 400)
+ sleep(1ms)
+ Tester2.goto(900, 800)
+ sleep(1000ms)
+ Tester1.goto(700, 500)
+ sleep(1ms)
+ Tester2.goto(1000, 900)
+ sleep(1000ms)
+ Tester1.goto(800, 600)
+ sleep(1ms)
+ Tester2.goto(1100, 1000)
+ sleep(2000ms)
+ print"if you got to the end you can run any of the games on ExbosSahara"
+ sleep(2000ms)
+ open(/ExbosSahara/main.py)
+ ")
+ 
+ 
+
+ 
+
+
+
+
