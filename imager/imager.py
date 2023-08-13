ba174cf2f422a7c0e59b4a96eb953754c0fb6aa4
SizzlRetreat <103748542+SizzlRetrat@users.noreply.github.com>
2023-08-13 10:43:02 +0100

    Update imager.py

--- imager/imager.py
+++ imager/imager.py
@@ -392,7 +392,135 @@ write(/Games/gamestest.py "
  if user == "EXIT"
  open(/ExbosSahara/main.py)
  ")
+open(/Games/thecorngame.py 'w')
+write(/Games/thecorngame.py "
+ import turtle
 
+ screen = turtle.Screen()
+ screen.title("The Corn Game type Corn to see your score anytime")
+ screen.bgcolor("green")
+
+ cornplant = turtle.Turtle()
+ cornplant.shape("triangle")
+ cornplant.color("yellow")
+ cornplant.shapesize(1, 1)
+ cornplant.penup()
+ cornplant.goto(0, 0)
+
+ corn = 0
+ sleep(1000ms)
+ corn += 1
+
+ #start101
+
+ if user == "exit"
+ open(/ExbosSahara/main.py)
+
+ if user == "corn"
+ print corn
+ goto start101
+
+ if corn == 100
+ cornplantplus = turtle.Turtle()
+ cornplantplus.shape("triangle")
+ cornplantplus.color("orange")
+ cornplantplus.shapesize(1, 2)
+ cornplantplus.penup()
+ cornplantplus.goto(0, 1)
+ sleep(100ms)
+ corn += 1
+ goto start101
+
+ if corn == 1000
+ cornplantpro = turtle.Turtle
+ cornplantpro.shape("triangle")
+ cornplantpro.color("red")
+ cornplantpro.shapesize(1, 3)
+ cornplantpro.penup()
+ cornplantpro.goto(0, 2)
+ sleep(10ms)
+ corn += 1
+ goto start101
+
+ if corn == 1000000
+ cornplantultimate = turtle.Turtle
+ cornplantultimate.shape("triangle")
+ cornplantultimate.color("pink")
+ cornplantultimate.shapesize(1, 4)
+ cornplantultimate.penup()
+ cornplantultimate.goto(0, 3)
+ sleep(1ms)
+ corn += 1
+ goto start101
+
+ if corn == 10000000000000
+ cornplantmax = turtle.Turtle
+ cornplantmax.shape("triangle")
+ cornplantmax.color("purple")
+ cornplantmax.shapesize(1, 5)
+ cornplantmax.penup()
+ cornplantmax.goto(0, 4)
+ sleep(0.1ms)
+ corn += 1
+ goto start101
+
+ if corn == 100000000000000000000
+ cornplantup = turtle.Turtle
+ cornplantup.shape("triangle")
+ cornplantup.color("blue")
+ cornplantup.shapesize(1, 6)
+ cornplantup.penup()
+ cornplantup.goto(0, 5)
+ sleep(0.01ms)
+ corn += 1
+ goto start101
+
+
+ if corn == 1000000000000000000000000000000000000
+ cornplant1 = turtle.Turtle
+ cornplant1.shape("triangle")
+ cornplant1.color("lightblue")
+ cornplant1.shapesize(1, 7)
+ cornplant1.penup()
+ cornplant1.goto(0, 6)
+ sleep(0.001ms)
+ corn += 1
+ goto start101
+
+ if corn == 10000000000000000000000000000000000000000000
+ cornplantdos = turtle.Turtle
+ cornplantdos.shape("triangle")
+ cornplantdos.color("blue")
+ cornplantdos.shapesize(1, 8)
+ cornplantdos.penup()
+ cornplantdos.goto(0, 7)
+ sleep(0.0001)
+ corn += 1
+ goto start101
+
+ if corn == 1000000000000000000000000000000000000000000000000000
+ cornplantdosa = turtle.Turtle
+ cornplantdosa.shape("triangle")
+ cornplantdosa.color("blue")
+ cornplantdosa.shapesize(1, 9)
+ cornplantdosa.penup()
+ cornplantdosa.goto(0, 8)
+ sleep(0.0001)
+ corn += 1
+ goto start101
+
+ if corn == 10000000000000000000000000000000000000000000000000000000000
+ cornplanttri = turtle.Turtle
+ cornplanttri.shape("triangle")
+ cornplanttri.color("green")
+ cornplanttri.shapesize(1, 10)
+ cornplanttri.penup()
+ cornplanttri.goto(0, 9)
+ sleep(0.00001)
+ corn += 1
+ print corn
+ goto start101
+")
 
 
  
